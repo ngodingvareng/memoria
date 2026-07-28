bf868a319690947d4c7c491e432f8052bc39e018
moez-rd <rahmat.ge4@gmail.com>
2026-07-28 12:31:30 +0700

    refactor(web): move form to it's components

--- web/src/routes/_app/activity.new.tsx
+++ web/src/routes/_app/activity.new.tsx
@@ -1,50 +1,12 @@
-import { Button } from '@/components/ui/button';
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from '@/components/ui/field';
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-  InputGroupText,
-} from '@/components/ui/input-group';
 import Wrapper from '@/components/wrapper';
-import { useForm } from '@tanstack/react-form';
+import { CreateActivityForm } from '@/features/activities';
 import { createFileRoute } from '@tanstack/react-router';
-import * as z from 'zod';
 
 export const Route = createFileRoute('/_app/activity/new')({
   component: RouteComponent,
 });
 
-const formSchema = z.object({
-  title: z
-    .string()
-    .min(5, 'Bug title must be at least 5 characters.')
-    .max(32, 'Bug title must be at most 32 characters.'),
-  description: z
-    .string()
-    .min(20, 'Description must be at least 20 characters.')
-    .max(100, 'Description must be at most 100 characters.'),
-});
-
 function RouteComponent() {
-  const form = useForm({
-    defaultValues: {
-      title: '',
-      description: '',
-    },
-    validators: {
-      onSubmit: formSchema,
-    },
-    onSubmit: async ({ value }) => {
-      console.log(value);
-    },
-  });
-
   return (
     <Wrapper>
       <div className="flex flex-col gap-8 max-w-xl">
@@ -52,53 +14,7 @@ function RouteComponent() {
           <h1 className="text-2xl font-semibold">Create activity</h1>
         </div>
 
-        <form
-          id="bug-report-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            form.handleSubmit();
-          }}
-        >
-          <FieldGroup>
-            <form.Field
-              name="title"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field data-invalid={isInvalid}>
-                    <FieldLabel htmlFor={field.name}>Title</FieldLabel>
-                    <InputGroup>
-                      <InputGroupInput
-                        id={field.name}
-                        name={field.name}
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        aria-invalid={isInvalid}
-                        placeholder="My journey"
-                        autoComplete="off"
-                      />
-                      <InputGroupAddon align="inline-end">
-                        <InputGroupText className="tabular-nums">
-                          {field.state.value.length}/100
-                        </InputGroupText>
-                      </InputGroupAddon>
-                    </InputGroup>
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
-              }}
-            />
-          </FieldGroup>
-        </form>
-        <Field orientation="horizontal">
-          <Button type="submit" form="bug-report-form">
-            Create
-          </Button>
-        </Field>
+        <CreateActivityForm />
       </div>
     </Wrapper>
   );
